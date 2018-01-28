36f24d54b519db064503aa63550a2e32059c1cef
Hasan Mahmud <hasan404cse@gmail.com>
2018-01-28 20:53:52 +0600

    Add files via upload

---
+++ Print after 2 alphabet.asm
@@ -0,0 +1,55 @@
+;   "Know Yourself"
+;	Slack_Coder(Mashod Rana)
+;	University of Asia Pacific
+;==========================================
+
+.MODEL SMALL
+.DATA    
+ 
+   CR EQU 0DH
+   NL EQU 0AH 
+   MSG DB "Give a character: $"
+   MSG2 DB NL,CR,"Output: "
+   VAL DB ?,"$"
+ 
+.STACK
+.CODE
+ 
+MAIN PROC
+      MOV AX,@DATA
+      MOV DS,AX
+ 
+ 
+      LEA DX,MSG
+      MOV AH,9
+      INT 21H
+ 
+      MOV AH,1
+      INT 21H
+ 
+      ADD AL,2
+ 
+      MOV BL,26
+      SUB AL,'a'
+ 
+      XOR AH,AH
+      DIV BL
+ 
+        MOV VAL,AH
+        ADD VAL,'a'
+ 
+        LEA DX,MSG2
+        MOV AH,9
+        INT 21H
+ 
+ 
+ 
+ 
+ 
+ 
+MAIN ENDP
+ 
+END  MAIN  
+ 
+ 
+RET
